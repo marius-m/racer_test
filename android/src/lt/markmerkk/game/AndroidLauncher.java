7d54d709f851e85cb79f81d16b1d670c8dd324d1
Marius Merkevičius <merkevicius.marius@gmail.com>
2016-06-04 00:22:26 +0300

    Initial project commit.

---
+++ android/src/lt/markmerkk/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package lt.markmerkk.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import lt.markmerkk.game.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
