bf60bc977ffa65098032e76cf8e8e9aa00586c7d
Marius Merkevicius <mariusmerkevicius@Mariuss-MacBook-Pro-2.local>
2016-08-28 13:45:16 +0300

    Minor fixes for syntax to run game

--- android/src/lt/markmerkk/game/AndroidLauncher.java
+++ android/src/lt/markmerkk/game/AndroidLauncher.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import lt.markmerkk.app.screens.GameScreen;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGdxGame(), config);
+        // fixme no initializer for android
+//		initialize(GameScreen(), config);
 	}
 }
