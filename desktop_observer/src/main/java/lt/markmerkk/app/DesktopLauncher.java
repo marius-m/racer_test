ff0981de5cbfa1546206f178978fc63348c5bc3a
Marius Merkevicius <mariusmerkevicius@Mariuss-MacBook-Pro-2.local>
2016-10-19 00:58:51 +0300

    Add observer client

---
+++ desktop_observer/src/main/java/lt/markmerkk/app/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package lt.markmerkk.app;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author mariusmerkevicius
+ * @since 2016-10-18
+ */
+public class DesktopLauncher {
+    public static void main(String[] args) {
+
+    }
+
+}
